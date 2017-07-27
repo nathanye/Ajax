c7bda6a92f3faef9d295eae05cd6ce7b76e7d5c0
nathanye <khalil54781@gmail.com>
2017-07-27 09:46:18 +0800

    ajax#like&dislike

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,6 @@ class PostsController < ApplicationController
       Like.create( :user => current_user, :post => @post )
     end
 
-    redirect_to posts_path
   end
 
   def unlike
@@ -33,7 +32,7 @@ class PostsController < ApplicationController
     like = @post.find_like(current_user)
     like.destroy
 
-    redirect_to posts_path
+    render "like"
   end
 
 
